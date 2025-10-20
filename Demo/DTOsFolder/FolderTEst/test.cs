059b999f07b09b84065dabc5da21b12193916362
Adel-abc <3adel.0523056@gmail.com>
2025-10-20 08:38:32 +0300

    heelo

--- Demo/DTOsFolder/FolderTEst/test.cs
+++ Demo/DTOsFolder/FolderTEst/test.cs
@@ -5,6 +5,7 @@
 
         public int MyProperty { get; set; }
         public int MyProperty2 { get; set; }
-        public int MyProperty3 { get; set;
+        public int MyProperty3 { get; set; }
+        public int MyProperty4 { get; set; }
     }
 }
